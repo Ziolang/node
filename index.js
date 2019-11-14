056cea6c095ef69d92cdc22255796391265fe672
stumpyfletcher@gmail.com <stumpyfletcher@gmail.com>
2019-11-14 10:24:05 -0700

    yay

--- index.js
+++ index.js
@@ -7,5 +7,39 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/mail')
+  .get('/mail', function(req, res){
+    console.log("Mail Function");
+    let op1 = Number(req.query.op1)
+    let op2 = Number(req.query.op2)
+    console.log(op1);
+    console.log(op2);
+    var operator = req.query.operator
+    console.log(operator);
+    var result = 0;
+    switch (operator) {
+      case "+":
+        console.log("Addition!");
+        result = op1 + op2;
+        break;
+      case "-":
+        console.log("Subtraction!");
+        result = op1 - op2;
+        break;
+      case "/":
+        console.log("Division!");
+        result = op1 / op2;
+        break;
+      case "*":
+        console.log("Multiplication!");
+        result = op1 * op2;
+        break;
+      default:
+        console.log("Broken!");
+        break;
+    }
+    console.log(result);
+    res.render("pages/results", {
+      result: result
+    })
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
